d41ce92ad5866a4c7eb4d4071c60cb13bf724272
NatiSen <ola.eufaloportugues@gmail.com>
2025-08-08 19:05:09 +0000

    Domain Name Generator Project Extra

--- src/app.js
+++ src/app.js
@@ -11,13 +11,37 @@ import "./assets/img/4geeks.ico";
 window.onload = function () {
   let pronoun = ['the', 'our'];
   let adj = ['great', 'big'];
-  let noun = ['jogger', 'racoon'];
+  let noun = ['jogger', 'racoon', 'lastofus'];
+
+  let extensions = ['.com', '.net', '.us', '.io'];
+
+  const list = document.getElementById('domain-list');
 
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
-        let domain = pronoun[i] + adj[j] + noun[k] + ".com";
-        console.log(domain);
+        let baseDomain = pronoun[i] + adj[j] + noun[k];
+
+        for (let e = 0; e < extensions.length; e++) {
+          let domain = baseDomain + extensions[e];
+
+       
+
+          let ext = extensions[e].slice(1); 
+
+          if (baseDomain.toLowerCase().endsWith(ext) && ext !== 'com') {
+         
+            domain = baseDomain.slice(0, -ext.length) + '.' + ext;
+          }
+
+          console.log(domain);
+
+          if (list) {
+            let li = document.createElement('li');
+            li.textContent = domain;
+            list.appendChild(li);
+          }
+        }
       }
     }
   }
